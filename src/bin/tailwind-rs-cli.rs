006b2c0590f7c3a7fe7088bbc55b61adbdac5cd7
Nifou <nifou@mailfence.com>
2022-04-27 18:32:00 +0200

    fix: fix binary glob pattern for simple paths

--- src/bin/tailwind-rs-cli.rs
+++ src/bin/tailwind-rs-cli.rs
@@ -1,15 +1,24 @@
-use std::{env, time::Instant};
-use wax::Glob;
+use std::{env, iter, path::PathBuf, time::Instant};
 use tailwind_rs::TailwindGenerator;
+use wax::Glob;
 
 fn main() {
     // TODO: Clap (multiple input paths, output file, config file, ...)
     let args: Vec<String> = env::args().collect();
-    let (prefix, glob) = Glob::partitioned(&args[1]).unwrap();
-
-    let start = Instant::now();
     let mut generator = TailwindGenerator::new();
-    generator.scan_files(glob.walk(prefix, usize::MAX).map(|e| e.unwrap().into_path()));
+
+    let (prefix, glob) = Glob::partitioned(&args[1]).unwrap();
+    let path = PathBuf::from(&args[1]);
+    let start = Instant::now();
+
+    if prefix == path {
+        generator.scan_files(iter::once(path));
+    } else {
+        generator.scan_files(
+            glob.walk(prefix, usize::MAX)
+                .map(|e| e.unwrap().into_path()),
+        );
+    }
 
     let css = generator.generate();
     let duration = start.elapsed();
