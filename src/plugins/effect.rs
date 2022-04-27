26b1e08f7e3a55e20be0c0a277fa0902f4a23872
Nifou <nifou@mailfence.com>
2022-04-27 18:12:50 +0200

    feat: support all effect utilities

--- src/plugins/effect.rs
+++ src/plugins/effect.rs
@@ -35,6 +35,37 @@ impl Plugin for MixBlendModePlugin {
 }
 
 #[derive(Debug)]
+pub struct BackgroundBlendModePlugin;
+
+impl Plugin for BackgroundBlendModePlugin {
+    fn namespace(&self) -> &str {
+        "bg-blend"
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        match modifier {
+            "normal" => write!(css_content, "background-blend-mode: normal;"),
+            "multiply" => write!(css_content, "background-blend-mode: multiply;"),
+            "screen" => write!(css_content, "background-blend-mode: screen;"),
+            "overlay" => write!(css_content, "background-blend-mode: overlay;"),
+            "darken" => write!(css_content, "background-blend-mode: darken;"),
+            "lighten" => write!(css_content, "background-blend-mode: lighten;"),
+            "color-dodge" => write!(css_content, "background-blend-mode: color-dodge;"),
+            "color-burn" => write!(css_content, "background-blend-mode: color-burn;"),
+            "hard-light" => write!(css_content, "background-blend-mode: hard-light;"),
+            "soft-light" => write!(css_content, "background-blend-mode: soft-light;"),
+            "difference" => write!(css_content, "background-blend-mode: difference;"),
+            "exclusion" => write!(css_content, "background-blend-mode: exclusion;"),
+            "hue" => write!(css_content, "background-blend-mode: hue;"),
+            "saturation" => write!(css_content, "background-blend-mode: saturation;"),
+            "color" => write!(css_content, "background-blend-mode: color;"),
+            "luminosity" => write!(css_content, "background-blend-mode: luminosity;"),
+            _ => Ok(()),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct BoxShadowPlugin;
 
 impl Plugin for BoxShadowPlugin {
@@ -112,82 +143,20 @@ impl Plugin for BoxShadowColorPlugin {
     }
 }
 
-/*use super::SelectorList;
+#[derive(Debug)]
+pub struct OpacityPlugin;
 
-pub fn init(selectors: &mut SelectorList) {
-    selectors.register("opacity-0", "opacity: 0;".to_string());
-    selectors.register("opacity-5", "opacity: 0.05;".to_string());
-    selectors.register("opacity-10", "opacity: 0.1;".to_string());
-    selectors.register("opacity-20", "opacity: 0.2;".to_string());
-    selectors.register("opacity-25", "opacity: 0.25;".to_string());
-    selectors.register("opacity-30", "opacity: 0.3;".to_string());
-    selectors.register("opacity-40", "opacity: 0.4;".to_string());
-    selectors.register("opacity-50", "opacity: 0.5;".to_string());
-    selectors.register("opacity-60", "opacity: 0.6;".to_string());
-    selectors.register("opacity-70", "opacity: 0.7;".to_string());
-    selectors.register("opacity-75", "opacity: 0.75;".to_string());
-    selectors.register("opacity-80", "opacity: 0.8;".to_string());
-    selectors.register("opacity-90", "opacity: 0.9;".to_string());
-    selectors.register("opacity-100", "opacity: 1;".to_string());
-    selectors.register(
-        "bg-blend-normal",
-        "background-blend-mode: normal;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-multiply",
-        "background-blend-mode: multiply;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-screen",
-        "background-blend-mode: screen;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-overlay",
-        "background-blend-mode: overlay;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-darken",
-        "background-blend-mode: darken;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-lighten",
-        "background-blend-mode: lighten;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-color-dodge",
-        "background-blend-mode: color-dodge;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-color-burn",
-        "background-blend-mode: color-burn;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-hard-light",
-        "background-blend-mode: hard-light;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-soft-light",
-        "background-blend-mode: soft-light;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-difference",
-        "background-blend-mode: difference;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-exclusion",
-        "background-blend-mode: exclusion;".to_string(),
-    );
-    selectors.register("bg-blend-hue", "background-blend-mode: hue;".to_string());
-    selectors.register(
-        "bg-blend-saturation",
-        "background-blend-mode: saturation;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-color",
-        "background-blend-mode: color;".to_string(),
-    );
-    selectors.register(
-        "bg-blend-luminosity",
-        "background-blend-mode: luminosity;".to_string(),
-    );
-}*/
+impl Plugin for OpacityPlugin {
+    fn namespace(&self) -> &str {
+        "opacity"
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        // NOTE: Not-compatible with TailwindCSS, support all values
+        if let Ok(opacity_value) = modifier.parse::<f32>() {
+            write!(css_content, "opacity: {};", opacity_value / 100.)
+        } else {
+            Ok(())
+        }
+    }
+}
