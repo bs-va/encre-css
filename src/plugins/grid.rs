5253a277b82ebab51739e5520cd177e8b2010ea8
Nifou <nifou@mailfence.com>
2022-04-29 08:29:49 +0200

    feat: support all grid utilities

--- src/plugins/grid.rs
+++ src/plugins/grid.rs
@@ -64,6 +64,187 @@ impl Plugin for RowsPlugin {
 }
 
 #[derive(Debug)]
+pub struct StartEndSpanColumnPlugin;
+
+impl Plugin for StartEndSpanColumnPlugin {
+    fn namespace(&self) -> &str {
+        "col"
+    }
+
+    fn is_matching_value(&self, _hint: &str, _val: &str) -> bool {
+        true
+    }
+
+    fn css_template_value(&self, val: &str, css_content: &mut String) -> Result {
+        write!(css_content, "grid-column: {val};")
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        if modifier == "auto" {
+            return self.css_template_value("auto", css_content);
+        }
+
+        if let Some(val) = modifier.strip_prefix("span-") {
+            if val == "full" {
+                return self.css_template_value("1 / -1", css_content);
+            }
+
+            // NOTE: Not-compatible with TailwindCSS, support all values
+            if let Ok(span_num) = val.parse::<usize>() {
+                return self.css_template_value(&format!("span {span_num} / span {span_num}"), css_content);
+            }
+        } else if let Some(val) = modifier.strip_prefix("start-") {
+            if val == "auto" {
+                return write!(css_content, "grid-column-start: auto;");
+            }
+
+            // NOTE: Not-compatible with TailwindCSS, support all values
+            if val.parse::<usize>().is_ok() {
+                return write!(css_content, "grid-column-start: {val};");
+            }
+        } else if let Some(val) = modifier.strip_prefix("end-") {
+            if val == "auto" {
+                return write!(css_content, "grid-column-end: auto;");
+            }
+
+            // NOTE: Not-compatible with TailwindCSS, support all values
+            if val.parse::<usize>().is_ok() {
+                return write!(css_content, "grid-column-end: {val};");
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct StartEndSpanRowPlugin;
+
+impl Plugin for StartEndSpanRowPlugin {
+    fn namespace(&self) -> &str {
+        "row"
+    }
+
+    fn is_matching_value(&self, _hint: &str, _val: &str) -> bool {
+        true
+    }
+
+    fn css_template_value(&self, val: &str, css_content: &mut String) -> Result {
+        write!(css_content, "grid-row: {val};")
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        if modifier == "auto" {
+            return self.css_template_value("auto", css_content);
+        }
+
+        if let Some(val) = modifier.strip_prefix("span-") {
+            if val == "full" {
+                return self.css_template_value("1 / -1", css_content);
+            }
+
+            // NOTE: Not-compatible with TailwindCSS, support all values
+            if let Ok(span_num) = val.parse::<usize>() {
+                return self.css_template_value(&format!("span {span_num} / span {span_num}"), css_content);
+            }
+        } else if let Some(val) = modifier.strip_prefix("start-") {
+            if val == "auto" {
+                return write!(css_content, "grid-row-start: auto;");
+            }
+
+            // NOTE: Not-compatible with TailwindCSS, support all values
+            if val.parse::<usize>().is_ok() {
+                return write!(css_content, "grid-row-start: {val};");
+            }
+        } else if let Some(val) = modifier.strip_prefix("end-") {
+            if val == "auto" {
+                return write!(css_content, "grid-row-end: auto;");
+            }
+
+            // NOTE: Not-compatible with TailwindCSS, support all values
+            if val.parse::<usize>().is_ok() {
+                return write!(css_content, "grid-row-end: {val};");
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct AutoFlowPlugin;
+
+impl Plugin for AutoFlowPlugin {
+    fn namespace(&self) -> &str {
+        "grid-flow"
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        match modifier {
+            "row" => write!(css_content, "grid-auto-flow: row;"),
+            "col" => write!(css_content, "grid-auto-flow: column;"),
+            "row-dense" => write!(css_content, "grid-auto-flow: row dense;"),
+            "col-dense" => write!(css_content, "grid-auto-flow: column dense;"),
+            _ => Ok(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AutoColumnsPlugin;
+
+impl Plugin for AutoColumnsPlugin {
+    fn namespace(&self) -> &str {
+        "auto-cols"
+    }
+
+    fn is_matching_value(&self, _hint: &str, _val: &str) -> bool {
+        true
+    }
+
+    fn css_template_value(&self, val: &str, css_content: &mut String) -> Result {
+        write!(css_content, "grid-auto-columns: {val};")
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        match modifier {
+            "auto" => self.css_template_value("auto", css_content),
+            "min" => self.css_template_value("min-content", css_content),
+            "max" => self.css_template_value("max-content", css_content),
+            "fr" => self.css_template_value("minmax(0, 1fr)", css_content),
+            _ => Ok(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AutoRowsPlugin;
+
+impl Plugin for AutoRowsPlugin {
+    fn namespace(&self) -> &str {
+        "auto-rows"
+    }
+
+    fn is_matching_value(&self, _hint: &str, _val: &str) -> bool {
+        true
+    }
+
+    fn css_template_value(&self, val: &str, css_content: &mut String) -> Result {
+        write!(css_content, "grid-auto-rows: {val};")
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        match modifier {
+            "auto" => self.css_template_value("auto", css_content),
+            "min" => self.css_template_value("min-content", css_content),
+            "max" => self.css_template_value("max-content", css_content),
+            "fr" => self.css_template_value("minmax(0, 1fr)", css_content),
+            _ => Ok(()),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct GapPlugin;
 
 impl Plugin for GapPlugin {
@@ -88,154 +269,52 @@ impl Plugin for GapPlugin {
     }
 }
 
-/*use super::SelectorList;
+#[derive(Debug)]
+pub struct GapXPlugin;
 
-pub fn init(selectors: &mut SelectorList) {
-    selectors.register("col-auto", "".to_string());
-    selectors.register("col-span-1", "".to_string());
-    selectors.register("col-span-2", "".to_string());
-    selectors.register("col-span-3", "".to_string());
-    selectors.register("col-span-4", "".to_string());
-    selectors.register("col-span-5", "".to_string());
-    selectors.register("col-span-6", "".to_string());
-    selectors.register("col-span-7", "".to_string());
-    selectors.register("col-span-8", "".to_string());
-    selectors.register("col-span-9", "".to_string());
-    selectors.register("col-span-10", "".to_string());
-    selectors.register("col-span-11", "".to_string());
-    selectors.register("col-span-12", "".to_string());
-    selectors.register("col-start-1", "".to_string());
-    selectors.register("col-start-2", "".to_string());
-    selectors.register("col-start-3", "".to_string());
-    selectors.register("col-start-4", "".to_string());
-    selectors.register("col-start-5", "".to_string());
-    selectors.register("col-start-6", "".to_string());
-    selectors.register("col-start-7", "".to_string());
-    selectors.register("col-start-8", "".to_string());
-    selectors.register("col-start-9", "".to_string());
-    selectors.register("col-start-10", "".to_string());
-    selectors.register("col-start-11", "".to_string());
-    selectors.register("col-start-12", "".to_string());
-    selectors.register("col-start-13", "".to_string());
-    selectors.register("col-start-auto", "".to_string());
-    selectors.register("col-end-1", "".to_string());
-    selectors.register("col-end-2", "".to_string());
-    selectors.register("col-end-3", "".to_string());
-    selectors.register("col-end-4", "".to_string());
-    selectors.register("col-end-5", "".to_string());
-    selectors.register("col-end-6", "".to_string());
-    selectors.register("col-end-7", "".to_string());
-    selectors.register("col-end-8", "".to_string());
-    selectors.register("col-end-9", "".to_string());
-    selectors.register("col-end-10", "".to_string());
-    selectors.register("col-end-11", "".to_string());
-    selectors.register("col-end-12", "".to_string());
-    selectors.register("col-end-13", "".to_string());
-    selectors.register("col-end-auto", "".to_string());
-    selectors.register("grid-rows-1", "".to_string());
-    selectors.register("grid-rows-2", "".to_string());
-    selectors.register("grid-rows-3", "".to_string());
-    selectors.register("grid-rows-4", "".to_string());
-    selectors.register("grid-rows-5", "".to_string());
-    selectors.register("grid-rows-6", "".to_string());
-    selectors.register("grid-rows-none", "".to_string());
-    selectors.register("row-auto", "".to_string());
-    selectors.register("row-span-1", "".to_string());
-    selectors.register("row-span-2", "".to_string());
-    selectors.register("row-span-3", "".to_string());
-    selectors.register("row-span-4", "".to_string());
-    selectors.register("row-span-5", "".to_string());
-    selectors.register("row-span-6", "".to_string());
-    selectors.register("row-start-1", "".to_string());
-    selectors.register("row-start-2", "".to_string());
-    selectors.register("row-start-3", "".to_string());
-    selectors.register("row-start-4", "".to_string());
-    selectors.register("row-start-5", "".to_string());
-    selectors.register("row-start-6", "".to_string());
-    selectors.register("row-start-7", "".to_string());
-    selectors.register("row-start-auto", "".to_string());
-    selectors.register("row-end-1", "".to_string());
-    selectors.register("row-end-2", "".to_string());
-    selectors.register("row-end-3", "".to_string());
-    selectors.register("row-end-4", "".to_string());
-    selectors.register("row-end-5", "".to_string());
-    selectors.register("row-end-6", "".to_string());
-    selectors.register("row-end-7", "".to_string());
-    selectors.register("row-end-auto", "".to_string());
-    selectors.register("gap-x-0", "".to_string());
-    selectors.register("gap-x-0.5", "".to_string());
-    selectors.register("gap-x-1", "".to_string());
-    selectors.register("gap-x-1.5", "".to_string());
-    selectors.register("gap-x-2", "".to_string());
-    selectors.register("gap-x-2.5", "".to_string());
-    selectors.register("gap-x-3", "".to_string());
-    selectors.register("gap-x-3.5", "".to_string());
-    selectors.register("gap-x-4", "".to_string());
-    selectors.register("gap-x-5", "".to_string());
-    selectors.register("gap-x-6", "".to_string());
-    selectors.register("gap-x-8", "".to_string());
-    selectors.register("gap-x-10", "".to_string());
-    selectors.register("gap-x-11", "".to_string());
-    selectors.register("gap-x-12", "".to_string());
-    selectors.register("gap-x-14", "".to_string());
-    selectors.register("gap-x-16", "".to_string());
-    selectors.register("gap-x-20", "".to_string());
-    selectors.register("gap-x-24", "".to_string());
-    selectors.register("gap-x-28", "".to_string());
-    selectors.register("gap-x-32", "".to_string());
-    selectors.register("gap-x-36", "".to_string());
-    selectors.register("gap-x-40", "".to_string());
-    selectors.register("gap-x-44", "".to_string());
-    selectors.register("gap-x-48", "".to_string());
-    selectors.register("gap-x-52", "".to_string());
-    selectors.register("gap-x-56", "".to_string());
-    selectors.register("gap-x-64", "".to_string());
-    selectors.register("gap-x-72", "".to_string());
-    selectors.register("gap-x-80", "".to_string());
-    selectors.register("gap-x-96", "".to_string());
-    selectors.register("gap-x-px", "".to_string());
-    selectors.register("gap-y-0", "".to_string());
-    selectors.register("gap-y-0.5", "".to_string());
-    selectors.register("gap-y-1", "".to_string());
-    selectors.register("gap-y-1.5", "".to_string());
-    selectors.register("gap-y-2", "".to_string());
-    selectors.register("gap-y-2.5", "".to_string());
-    selectors.register("gap-y-3", "".to_string());
-    selectors.register("gap-y-3.5", "".to_string());
-    selectors.register("gap-y-4", "".to_string());
-    selectors.register("gap-y-5", "".to_string());
-    selectors.register("gap-y-6", "".to_string());
-    selectors.register("gap-y-8", "".to_string());
-    selectors.register("gap-y-10", "".to_string());
-    selectors.register("gap-y-11", "".to_string());
-    selectors.register("gap-y-12", "".to_string());
-    selectors.register("gap-y-14", "".to_string());
-    selectors.register("gap-y-16", "".to_string());
-    selectors.register("gap-y-20", "".to_string());
-    selectors.register("gap-y-24", "".to_string());
-    selectors.register("gap-y-28", "".to_string());
-    selectors.register("gap-y-32", "".to_string());
-    selectors.register("gap-y-36", "".to_string());
-    selectors.register("gap-y-40", "".to_string());
-    selectors.register("gap-y-44", "".to_string());
-    selectors.register("gap-y-48", "".to_string());
-    selectors.register("gap-y-52", "".to_string());
-    selectors.register("gap-y-56", "".to_string());
-    selectors.register("gap-y-64", "".to_string());
-    selectors.register("gap-y-72", "".to_string());
-    selectors.register("gap-y-80", "".to_string());
-    selectors.register("gap-y-96", "".to_string());
-    selectors.register("gap-y-px", "".to_string());
-    selectors.register("grid-flow-row", "".to_string());
-    selectors.register("grid-flow-col", "".to_string());
-    selectors.register("grid-flow-row-dense", "".to_string());
-    selectors.register("grid-flow-col-dense", "".to_string());
-    selectors.register("auto-cols-auto", "".to_string());
-    selectors.register("auto-cols-min", "".to_string());
-    selectors.register("auto-cols-max", "".to_string());
-    selectors.register("auto-cols-fr", "".to_string());
-    selectors.register("auto-rows-auto", "".to_string());
-    selectors.register("auto-rows-min", "".to_string());
-    selectors.register("auto-rows-max", "".to_string());
-    selectors.register("auto-rows-fr", "".to_string());
-}*/
+impl Plugin for GapXPlugin {
+    fn namespace(&self) -> &str {
+        "gap-x"
+    }
+
+    fn is_matching_value(&self, _hint: &str, val: &str) -> bool {
+        is_matching_length(val)
+    }
+
+    fn css_template_value(&self, val: &str, css_content: &mut String) -> Result {
+        write!(css_content, "column-gap: {val};")
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        if let Some(length) = default_lengths::get_basic(modifier) {
+            self.css_template_value(&length, css_content)
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct GapYPlugin;
+
+impl Plugin for GapYPlugin {
+    fn namespace(&self) -> &str {
+        "gap-y"
+    }
+
+    fn is_matching_value(&self, _hint: &str, val: &str) -> bool {
+        is_matching_length(val)
+    }
+
+    fn css_template_value(&self, val: &str, css_content: &mut String) -> Result {
+        write!(css_content, "row-gap: {val};")
+    }
+
+    fn get_css_for_modifier(&self, modifier: &str, css_content: &mut String) -> Result {
+        if let Some(length) = default_lengths::get_basic(modifier) {
+            self.css_template_value(&length, css_content)
+        } else {
+            Ok(())
+        }
+    }
+}
