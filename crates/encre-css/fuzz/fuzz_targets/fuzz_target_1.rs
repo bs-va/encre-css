2a7f1ca04df264a3cd26780a6395e6fd75f68c3e
Nifou <nifou@mailfence.com>
2025-07-08 10:47:53 +0200

    chore: improve fuzz testing

--- crates/encre-css/fuzz/fuzz_targets/fuzz_target_1.rs
+++ crates/encre-css/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -3,10 +3,7 @@
 use encre_css::Config;
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        let config = Config::default();
-
-        encre_css::generate([s], &config);
-    }
+fuzz_target!(|data: &str| {
+    let config = Config::default();
+    encre_css::generate([data], &config);
 });
